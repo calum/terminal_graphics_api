e80eff8953a0cc8516707bf8437afaeca729f4d2
calum <calumforster@live.co.uk>
2018-05-11 16:50:57 +0100

    fixed cargo warns

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,6 @@
 extern crate terminal_graphics;
 
 use terminal_graphics::{Display, Colour, Container, Text};
-use terminal_graphics::graphics::shapes::Rect;
 use terminal_graphics::graphics::Graphic;
 
 fn main() {
@@ -9,7 +8,7 @@ fn main() {
 
     let mut text = Text::from(2, 1, String::from("Title!"), Colour::Blue, Colour::Black);
 
-    let mut container = Container::from(10, 10, 10, 10, &mut text, Colour::BrightRed, Colour::BrightBlack);
+    let container = Container::from(10, 10, 10, 10, &mut text, Colour::BrightRed, Colour::BrightBlack);
 
     container.draw(&mut display);
 
